027b66f8ffc63c323a5d16817d5a9bcd4a380909
Sanes05 <kevinmauricejordan2005@gmail.com>
2024-09-08 03:24:35 +0200

    First Commir

--- Notizapp/ContentView.swift
+++ Notizapp/ContentView.swift
@@ -9,53 +9,20 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+    @State private var notes : [(title: String, text:String)]=[]
+    @State private var trashcan : [(title: String, text:String)]=[]
+    @State private var newNoteTitle : String = ""
+    @State private var newNoteText: String = ""
+    @State private var errorMessage: String = ""
+    var body:some View{
+        VStack{
+            TextField("Notiz Titel", text:$newNoteTitle)
+                .padding()
+                .background(Color.gray.opacity(0.2))
         }
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View{ContentView()}
 }
